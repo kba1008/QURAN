96bccc6634f689c71b3471e0c56288be5768f341
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-08-12 00:08:42 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - Navigasi HTML: Network First (sentiasa cuba versi terkini, fallback cache bila offline)
    - Aset statik: Cache First
 */
-const CACHE = "tasmi-v7";
+const CACHE = "tasmi-v8";
 const ASSETS = [
   "./",
   "./index.html",
