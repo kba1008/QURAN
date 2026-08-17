998d4efa1ddec35796daf98849ea554b88e930ca
kba1008 <sekolah-4896@moe-dl.edu.my>
2026-08-17 08:00:07 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    - Data Al-Quran  : Stale-While-Revalidate (disimpan untuk kegunaan luar talian)
    - Audio bacaan   : Cache First (diuruskan juga oleh app: 1 ayat ke hadapan)
 */
-const VERSION = "v39";
+const VERSION = "v40";
 const SHELL = "tasmi-shell-" + VERSION;
 const FONTS = "tasmi-fonts-" + VERSION;
 const DATA = "tasmi-data-" + VERSION;
@@ -16,10 +16,10 @@ const AUDIO = "tasmi-audio-v1"; // sama dengan nama cache dalam app
 const ASSETS = [
   "./",
   "./index.html",
-  "./wasalEngine.js?v=39",
+  "./wasalEngine.js?v=40",
   "./manifest.json",
-  "./icon-192.png?v=39",
-  "./icon-512.png?v=39",
+  "./icon-192.png?v=40",
+  "./icon-512.png?v=40",
 ];
 
 const KEEP = [SHELL, FONTS, DATA, AUDIO];
